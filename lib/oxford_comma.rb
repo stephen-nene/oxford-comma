39086a809a3297cf52aaf1690402d9808197966b
+stephen.nene <+>
2023-03-12 21:00:05 +0300

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_item = ", and " + array.pop
+    array.join(", ") + last_item
+  end
 end
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
